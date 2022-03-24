04578500c7cdfcef2d0872c0e0af96b1bd81c5e5
= <=>
2022-03-24 18:24:28 +0530

    my first commit of Maven Java Project

---
+++ MavenJava/src/main/java/rahulshettyacademy/MavenJava/App.java
@@ -0,0 +1,13 @@
+package rahulshettyacademy.MavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
